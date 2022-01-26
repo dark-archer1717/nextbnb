93c987af4f30e0470176ab667766ef6bcaf38516
Efazul Fayad <efazul.fayad1717@gmail.com>
2022-01-26 20:32:11 +0600

    17

--- model.js
+++ model.js
@@ -1,11 +1,12 @@
 import { Sequelize, Model, DataTypes } from 'sequelize'
+import bcrypt from 'bcrypt'
 
 const sequelize = new Sequelize(process.env.POSTGRES_CONNECTION, {
   dialect: 'postgres',
   logging: false
 })
 
-export class User extends Model { }
+class User extends Model { }
 
 User.init(
   {
@@ -27,6 +28,19 @@ User.init(
   {
     sequelize,
     modelName: 'user',
-    timestamps: false
+    timestamps: false,
+    hooks: {
+      beforeCreate: async (user) => {
+        const saltRounds = 10
+        const salt = await bcrypt.genSalt(saltRounds)
+        user.password = await bcrypt.hash(user.password, salt)
+      }
+    }
   }
 )
+
+User.prototype.isPasswordValid = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+export { sequelize, User }
