b82812f508b840f833f21a45ec4c02f1ceefa68e
Efazul Fayad <efazul.fayad1717@gmail.com>
2022-02-21 17:10:26 +0600

    mm

--- pages/host/new.js
+++ pages/host/new.js
@@ -1,53 +1,83 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import Layout from '../../components/Layout'
+import { useState } from 'react';
+import Head from 'next/head';
+import Layout from '../../components/Layout';
 
 const NewHouse = () => {
-  const [title, setTitle] = useState('')
+  const [title, setTitle] = useState('');
+  const [town, setTown] = useState('');
+  const [price, setPrice] = useState('');
+  const [picture, setpicture] = useState('');
+  const [description, setDescription] = useState('');
+  const [guests, setGuests] = useState(0);
+  const [bedrooms, setBedrooms] = useState();
+  const [bed, setBeds] = useState(0);
+  const [baths, setBaths] = useState(0);
+  const [wifi, setWifi] = useState(false);
+  const [kitchen, setKitchen] = useState(false);
+  const [heating, setHeating] = useState(false);
+  const [freeParking, setFreeParking] = useState(false);
+  const [entirePlace, setEntirePlace] = useState(false);
+  const [type, setType] = useState('Entire house');
+
+  const houseType = ['Entire house', 'Room'];
 
   return (
     <Layout
       content={
         <div>
           <Head>
-            <title>
-              Add a new house
-            </title>
+            <title>Add a new house</title>
           </Head>
           <form
             onSubmit={async (event) => {
-              event.preventDefault()
+              event.preventDefault();
               try {
                 const response = await axios.post('/api/host/new', {
                   house: {
                     title,
-                  },
-                })
+                    town,
+                    price,
+                    picture,
+                    description,
+                    guests,
+                    bedrooms,
+                    bed,
+                    baths,
+                    wifi,
+                    kitchen,
+                    heating,
+                    freeParking,
+                    entirePlace,
+                    type
+                  }
+                });
                 if (response.data.status === 'error') {
-                  alert(response.data.message)
-                  return
+                  alert(response.data.message);
+                  return;
                 }
 
-                console.log(response)
-                goto('/host')
+                console.log(response);
+                goto('/host');
               } catch (error) {
-                alert(error.response.data.message)
-                return
+                alert(error.response.data.message);
+                return;
               }
             }}
           >
             <input
               id="title"
               type="text"
-              placeholder='House Title'
-              onChange={(event) => setTitle(event.target.value)} />
-            <button>Add House</button>
-          </form >
+              placeholder="House title"
+              onChange={(event) => setTitle(event.target.value)}
+            />
+            <button>Add house</button>
+          </form>
+
           <style jsx>{``}</style>
-        </div >
+        </div>
       }
     />
-  )
-}
+  );
+};
 
-export default NewHouse
+export default NewHouse;
--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-import { Booking, User, House } from '../../../model.js'
+import { Booking, User, House } from '../../../model.js';
 
-server.get('/api/host/list', async (req, res) => {
+server.get('/api/host/index', async (req, res) => {
   if (!req.session.passport || !req.session.passport.user) {
     res.writeHead(403, {
-      'Content-Type': 'application/json',
-    })
+      'Content-Type': 'application/json'
+    });
     res.end(
       JSON.stringify({
         status: 'error',
-        message: 'Unauthorized',
+        message: 'Unauthorized'
       })
-    )
+    );
 
-    return
+    return;
   }
 
-  const userEmail = req.session.passport.user
-  const user = await User.findOne({ where: { email: userEmail } })
+  const userEmail = req.session.passport.user;
+  const user = await User.findOne({ where: { email: userEmail } });
 
   const houses = await House.findAll({
     where: {
-      host: user.id,
-    },
-  })
-  const houseIds = houses.map((house) => house.dataValues.id)
+      host: user.id
+    }
+  });
+  const houseIds = houses.map((house) => house.dataValues.id);
 
   const bookingsData = await Booking.findAll({
     where: {
       paid: true,
       houseId: {
-        [Op.in]: houseIds,
+        [Op.in]: houseIds
       },
       endDate: {
-        [Op.gte]: new Date(),
-      },
+        [Op.gte]: new Date()
+      }
     },
-    order: [['startDate', 'ASC']],
-  })
+    order: [['startDate', 'ASC']]
+  });
 
   const bookings = await Promise.all(
     bookingsData.map(async (booking) => {
@@ -44,47 +44,46 @@ server.get('/api/host/list', async (req, res) => {
         booking: booking.dataValues,
         house: houses.filter(
           (house) => house.dataValues.id === booking.dataValues.houseId
-        )[0].dataValues,
-      }
+        )[0].dataValues
+      };
     })
-  )
+  );
 
   res.writeHead(200, {
-    'Content-Type': 'application/json',
-  })
+    'Content-Type': 'application/json'
+  });
   res.end(
     JSON.stringify({
       bookings,
-      houses,
+      houses
     })
-  )
-})
+  );
+});
 
 server.post('/api/host/new', async (req, res) => {
-  const houseData = req.body.house
+  const houseData = req.body.house;
 
   if (!req.session.passport) {
     res.writeHead(403, {
-      'Content-Type': 'application/json',
-    })
+      'Content-Type': 'application/json'
+    });
     res.end(
       JSON.stringify({
         status: 'error',
-        message: 'Unauthorized',
+        message: 'Unauthorized'
       })
-    )
-
-    return
+    );
+    return;
   }
 
-  const userEmail = req.session.passport.user
+  const userEmail = req.session.passport.user;
   User.findOne({ where: { email: userEmail } }).then((user) => {
-    houseData.host = user.id
+    houseData.host = user.id;
     House.create(houseData).then(() => {
       res.writeHead(200, {
-        'Content-Type': 'application/json',
-      })
-      res.end(JSON.stringify({ status: 'success', message: 'ok' }))
-    })
-  })
-})
+        'Content-Type': 'application/json'
+      });
+      res.end(JSON.stringify({ status: 'success', message: 'ok' }));
+    });
+  });
+});
