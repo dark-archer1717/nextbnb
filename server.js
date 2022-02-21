a80fe47fe0470425ca7f2fb502907f7a655c85fa
Efazul Fayad <efazul.fayad1717@gmail.com>
2022-02-21 16:36:28 +0600

    server

--- server.js
+++ server.js
@@ -59,3 +59,32 @@ server.get('/api/host/list', async (req, res) => {
     })
   )
 })
+
+server.post('/api/host/new', async (req, res) => {
+  const houseData = req.body.house
+
+  if (!req.session.passport) {
+    res.writeHead(403, {
+      'Content-Type': 'application/json',
+    })
+    res.end(
+      JSON.stringify({
+        status: 'error',
+        message: 'Unauthorized',
+      })
+    )
+
+    return
+  }
+
+  const userEmail = req.session.passport.user
+  User.findOne({ where: { email: userEmail } }).then((user) => {
+    houseData.host = user.id
+    House.create(houseData).then(() => {
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      })
+      res.end(JSON.stringify({ status: 'success', message: 'ok' }))
+    })
+  })
+})
